966645fbc86be000af03e4a41faa09b01c86f687
Ignas Bernotas <ignas@iber.lt>
2018-08-07 10:51:25 +0300

    More specific FEATURES var flow type

--- src/main/dependencies/modules/application.js
+++ src/main/dependencies/modules/application.js
@@ -73,7 +73,8 @@ function bootstrap (container: Container) {
     'featureToggle',
     [],
     () => {
-      declare var FEATURES: any
+      declare var FEATURES: { [string]: boolean }
+
       return new FeatureToggle(FEATURES)
     }
   )
