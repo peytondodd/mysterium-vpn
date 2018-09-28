96b9e04a3666325bd38f4daec436bdbbc57dc933
Donatas Kucinskas <donce.lt@gmail.com>
2018-09-28 13:54:29 +0300

    Refactor communication utils specs

--- test/unit/app/communication/utils.spec.js
+++ test/unit/app/communication/utils.spec.js
@@ -15,24 +15,18 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// @flow
+
 import { onFirstEvent } from '../../../../src/app/communication/utils'
+import { describe, expect, it } from '../../../helpers/dependencies'
 
 const subscription = (onResolve) => onResolve('resolution of instant data')
-const subscriptionAsync = async (onResolve) => {
-  const data = await new Promise((resolve) => {
-    resolve('resolution of async data')
-  })
-  onResolve(data)
-}
 
-describe('onFirstEvent', () => {
-  it('resolves once serial data is passed to callback', async () => {
-    const resolvedData = await onFirstEvent(subscription)
-    expect(resolvedData).to.eql('resolution of instant data')
-  })
-
-  it('resolves once promise is resolved', async () => {
-    const resolvedData = await onFirstEvent(subscriptionAsync)
-    expect(resolvedData).to.eql('resolution of async data')
+describe('utils', () => {
+  describe('.onFirstEvent', () => {
+    it('resolves once serial data is passed to callback', async () => {
+      const resolvedData = await onFirstEvent(subscription)
+      expect(resolvedData).to.eql('resolution of instant data')
+    })
   })
 })
