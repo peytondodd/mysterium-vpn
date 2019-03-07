0e7e925581dd5c18f5a812f454369d7c7487180d
Donatas Kucinskas <donce.lt@gmail.com>
2019-03-07 17:25:19 +0200

    Fix binary name in daemon-uninstaller.sh

--- util_scripts/daemon-uninstaller.sh
+++ util_scripts/daemon-uninstaller.sh
@@ -3,4 +3,4 @@ sudo launchctl remove network.mysterium.mysteriumclient
 sudo launchctl stop /Library/LaunchDaemons/network.mysterium.mysteriumclient.plist
 sudo launchctl unload /Library/LaunchDaemons/network.mysterium.mysteriumclient.plist
 sudo rm -rf /Library/LaunchDaemons/network.mysterium.mysteriumclient.plist
-sudo killall mysterium_client
+sudo killall myst
